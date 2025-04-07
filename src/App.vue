2c0edc384ab660671fe31123afeae709e6ba3f2b
mmmyster <jakmalicher@gmail.com>
2025-04-07 21:22:06 +0200

    card has two faces and ability to flip

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
+import { ref } from 'vue'
 import Card from './components/Card.vue'
 
-const cardList = []
+const cardList = ref([])
 
 for (let index = 0; index < 16; index++) {
-  cardList.push(index)
+  cardList.value.push({ position: index, value: index, visible: false })
+}
+
+const flipCard = (payload) => {
+  cardList.value[payload.position].visible = true
 }
 </script>
 
@@ -12,7 +17,14 @@ for (let index = 0; index < 16; index++) {
   <h1>Memaze</h1>
 
   <section class="board">
-    <Card v-for="(card, index) in cardList" :key="`card-${index}`" :value="card" />
+    <Card
+      v-for="(card, index) in cardList"
+      :key="index"
+      :position="card.position"
+      :value="card.value"
+      :visible="card.visible"
+      @pick-card="flipCard"
+    />
   </section>
 </template>
 
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,57 @@
 <script setup>
 const props = defineProps({
+  position: {
+    type: Number,
+    required: true,
+  },
   value: {
     type: Number,
     required: true,
   },
+  visible: {
+    type: Boolean,
+    default: false,
+  },
 })
+
+const emit = defineEmits(['pick-card'])
+
+const pickCard = () => {
+  emit('pick-card', {
+    position: props.position,
+  })
+}
 </script>
 
 <template>
-  <div class="card">{{ value }}</div>
+  <div class="card" @click="pickCard">
+    <div v-if="visible" class="card-face face-up">
+      {{ value }}
+    </div>
+    <div v-else class="card-face face-down">down</div>
+  </div>
 </template>
 
-<style></style>
+<style>
+.card {
+  border: 1px solid #000;
+  background: rgb(242, 242, 242);
+  position: relative;
+}
+
+.card-face {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+
+.card-face.face-up {
+  background: #e64134;
+  color: #fff;
+}
+
+.card-face.face-down {
+  background: #3b9ae1;
+  color: #fff;
+}
+</style>
