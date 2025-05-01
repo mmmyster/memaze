a67178e988a23a8dd2f25f5468b35feceb6f3005
mmmyster <jakmalicher@gmail.com>
2025-05-01 19:51:44 +0200

    add space test task

--- src/data.js
+++ src/data.js
@@ -1,22 +1,22 @@
 export const data =
 {
-  "question": "Ktoré obrazy boli namaľované počas obdobia RENESANCIE?",
+  "question": "Ktoré planéty patria do slnečnej sústavy?",
   "tiles": [
-    { "id": 0, "answer": true, "content": "/img/960px-Brueghel-tower-of-babel.jpg" },
-    { "id": 1, "answer": true, "content": "/img/991px-Pieter_Bruegel_the_Elder_-_Children’s_Games_-_Google_Art_Project.jpg" },
-    { "id": 2, "answer": false, "content": "/img/carter_1996-9_o2_15.jpg" },
-    { "id": 3, "answer": false, "content": "/img/Albert_Bierstadt_-_Sunrise,_Yosemite_Valley_(c._1870).jpg" },
-    { "id": 4, "answer": false, "content": "/img/960px-Parson_Weems'_Fable.jpg" },
-    { "id": 5, "answer": true, "content": "/img/960px-_The_School_of_Athens__by_Raffaello_Sanzio_da_Urbino.jpg" },
-    { "id": 6, "answer": false, "content": "/img/Unusual_Views_of_Celebrated_Bridges_in_the_Provinces-Hietsu_No_Sakai_Tsurihashi.jpg" },
-    { "id": 7, "answer": false, "content": "/img/1024px-Alfred_Sisley_-_The_Lock_of_Saint-Mammès_(1885,_Nelson-Atkins_Museum).jpg" },
-    { "id": 8, "answer": false, "content": "/img/960px-Vincent_van_Gogh_-_De_slaapkamer_-_Google_Art_Project.jpg" },
-    { "id": 9, "answer": true, "content": "/img/1182px-Giovanni_Bellini_-_Allegoria_sacra.jpg" },
-    { "id": 10, "answer": true, "content": "/img/1280px-Creacion_de_Adam.jpg" },
-    { "id": 11, "answer": false, "content": "/img/960px-The_Night_Watch_-_HD.jpg" },
-    { "id": 12, "answer": false, "content": "/img/960px-The_Kiss_-_Gustav_Klimt_-_Google_Cultural_Institute.jpg" },
-    { "id": 13, "answer": false, "content": "/img/New_york_restaurant_by_edward_hopper.jpg" },
-    { "id": 14, "answer": true, "content": "/img/1146px-Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg" },
-    { "id": 15, "answer": false, "content": "/img/Dirck_Hals_-_Banquet_Scene_in_a_Renaissance_Hall_-_WGA11035.jpg" }
+    { "id": 0, "answer": true, "image": "/img/neptune.jpg", "text": "NEPTÚN" },
+    { "id": 1, "answer": true, "image": "/img/uranus.jpg", "text": "URÁN" },
+    { "id": 2, "answer": false, "image": "/img/pluto.jpg", "text": "PLUTO" },
+    { "id": 3, "answer": false, "image": "/img/comet.jpg", "text": "KOMÉTA" },
+    { "id": 4, "answer": true, "image": "/img/milky-way.jpg", "text": "" },
+    { "id": 5, "answer": true, "image": "/img/jupiter.jpg", "text": "JUPITER" },
+    { "id": 6, "answer": false, "image": "/img/mars.jpg", "text": "MARS" },
+    { "id": 7, "answer": false, "image": "/img/saturn.jpg", "text": "SATURN" },
+    { "id": 8, "answer": true, "image": "/img/moon.jpg", "text": "MESIAC" },
+    { "id": 9, "answer": true, "image": "/img/orbit.jpg", "text": "" },
+    { "id": 10, "answer": true, "image": "/img/venus.jpg", "text": "VENUŠA" },
+    { "id": 11, "answer": true, "image": "/img/mercury.jpg", "text": "MERKÚR" },
+    { "id": 12, "answer": false, "image": "/img/mars-bar.jpg", "text": "" },
+    { "id": 13, "answer": false, "image": "/img/space-rock.jpg", "text": "PLANÉTKA" },
+    { "id": 14, "answer": false, "image": "/img/earth.jpg", "text": "ZEM" },
+    { "id": 15, "answer": false, "image": "/img/sun.jpg", "text": "" }
   ]
 }
